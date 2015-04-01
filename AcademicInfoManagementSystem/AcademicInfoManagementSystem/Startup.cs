7a07ee0ad1f7b99dfa075ce8a53c1ca9ddbdee93
achimus <adrian.achim94@gmail.com>
2015-04-01 18:57:41 +0300

    Base Project

---
+++ AcademicInfoManagementSystem/AcademicInfoManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcademicInfoManagementSystem.Startup))]
+namespace AcademicInfoManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
