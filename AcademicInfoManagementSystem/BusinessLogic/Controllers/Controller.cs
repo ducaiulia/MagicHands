562c2faa5519c607ac8e45f0a5105e0ac7ebb8d8
achimus <adrian.achim94@gmail.com>
2015-05-09 13:32:07 +0300

    Fixes

---
+++ AcademicInfoManagementSystem/BusinessLogic/Controllers/Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using DatabaseEngine.Models;
+
+namespace BusinessLogic.Controllers
+{
+    public class Controller
+    {
+        #region Constructor & Instance
+        private Controller()
+        {
+
+        }
+
+        private static Controller instance;
+        public static Controller Instance
+        {
+            get { return instance ?? (instance = new Controller()); }
+        }
+
+        #endregion
+
+        #region Functionalities
+
+        public bool AddGrade(Exam exam, Student student, int grade)
+        {
+            if (exam.StudentExamGrades.Count(seg => seg.Exam == exam && seg.Student == student) > 0)
+                return false;
+            var examGrade = new StudentExamGrade(student, grade, exam);
+            student.GradesList.Add(examGrade);
+            exam.StudentExamGrades.Add(examGrade);
+            return true;
+        }
+        #endregion
+
+        #region Fields
+        #endregion
+    }
+}
